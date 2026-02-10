5d74c589f034b5fb2c0b81c279cb00abe4f271b9
Adenilson Silva <adenilsonsantos8891@gmail.com>
2026-02-09 22:14:37 -0300

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(3002, () => {
+  console.log('Server is running on port http://localhost:3002');
+});
+
+module.exports = app;
