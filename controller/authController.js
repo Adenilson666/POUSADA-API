64d30ad12bd2cbe7b50f198261c53883f2a901b7
Adenilson Silva <adenilsonsantos8891@gmail.com>
2026-02-17 22:12:57 -0300

    feat ajustes

--- controller/authController.js
+++ controller/authController.js
@@ -10,41 +10,7 @@ const loginUser = async (req, res, next) => {
     }
 };
 
-const getMe = async (req, res, next) => {
-    try {
-        const userId = req.user.id;
-        const user = await authService.getMe(userId);
-        res.status(200).json(user);
-    } catch (error) {
-        next(error);
-    }
-};
-
-const updateMe = async (req, res, next) => {
-    try {
-        const result = await authService.updateMe(req.user.id, req.body);
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
-};
-
-const changeMyPassword = async (req, res, next) => {
-    try {
-        // Implementation for changing password goes here
-        const { currentPassword, newPassword } = req.body;
-
-        await authService.changeMyPassword(req.user.id, {currentPassword, newPassword});
-
-        return res.status(204).send();
-    } catch (error) {
-        next(error);
-    }
-};
 
 module.exports = {
     loginUser,
-    getMe,
-    updateMe,
-    changeMyPassword
 };
